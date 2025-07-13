64990a1a22226fb1cbd02ff9390a149f50f78b65
Napat Poolpiputtanan <116888191+NapatPool@users.noreply.github.com>
2025-07-13 16:10:52 +0700

    Update Mini project SQL.sql

--- SQL/Mini project SQL.sql
+++ SQL/Mini project SQL.sql
@@ -1,4 +1,4 @@
---Load chinook (Database).db to https://sqliteonline.com/
+-- Load chinook (Database).db to https://sqliteonline.com/
 
 -- select firstname lastname from customer table and select country is USA, Brazil, France 
 -- Excel vs SQL: Filter = Where
@@ -31,6 +31,47 @@ SELECT
 	SUM(total) AS revenue, 
 	ROUND(SUM(total)*0.7,2) AS vat
 FROM invoices
+
+-- Select NULL data
+SELECT * FROM customers
+WHERE company IS NULL
+	
+-- Select not NULL data
+SELECT * FROM customers
+WHERE company IS NOT NULL
+
+-- How to use GROUP BY (ตัวที่ต้องการ GROUP BY จะต้องอยู่ใน SELECT ด้วย)
+SELECT 
+	country,
+    	COUNT(*)
+FROM customers
+GROUP by country;
+
+-- How to use ORDER BY (การเรียงข้อมูล จาก column ที่เราต้องการ) (DESC = เรียงจากมากไปน้อย)
+SELECT name, milliseconds, bytes
+FROM tracks
+ORDER BY bytes DESC
+
+-- How to use JOIN table (like VLOOK UP) (เอาค่าใน column มา match กัน) (pk=primary key)
+SELECT * FROM table1
+JOIN table2
+ON table1.pk = table2.fk; 
+
+-- INNER JOIN (default) = select only match 
+-- LEFT JOIN = table ซ้านตั้ง แล้วเอาอีก table มาต่อกัน ค่าไหน match ไม่ได้จะเป็น NULL
+SELECT * FROM artists
+JOIN albums
+ON artists.ArtistId = albums.ArtistId
+
+
+
+
+
+
+
+
+
+
 
 
 
