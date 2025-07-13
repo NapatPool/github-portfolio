ae8294ca7a246de2d3b089db5488683ca53f41b2
Napat Poolpiputtanan <116888191+NapatPool@users.noreply.github.com>
2025-07-13 15:43:25 +0700

    Update Mini project SQL.sql

--- SQL/Mini project SQL.sql
+++ SQL/Mini project SQL.sql
@@ -2,15 +2,15 @@
 
 -- select firstname lastname from customer table and select country is USA, Brazil, France 
 -- Excel vs SQL: Filter = Where
-select 
+SELECT 
 	firstname,
     	lastname,
 	firstname  || lastname AS fullname
 FROM customers
-WHERE country in ('USA','Brazil','France');
+WHERE country IN ('USA','Brazil','France');
 	
 --How to use pattern matching > Select whatever before "@yahoo" (use %)
-select 
+SELECT 
 	firstname AS fname, 
    	lastname AS lname, 
 	firstname,
@@ -19,6 +19,18 @@ FROM customers
 WHERE email LIKE '%@yahoo%';
 
 --How to use aggregate functions
+SELECT 
+	SUM(total) AS revenue, 
+	SUM(total) AS vat,
+	MIN(total),
+	MAX(total)
+FROM invoices
+
+-- Calculate VAT
+SELECT 
+	SUM(total) AS revenue, 
+	ROUND(SUM(total)*0.7,2) AS vat
+FROM invoices
 
 
 
