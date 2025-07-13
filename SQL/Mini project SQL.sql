eb0184b34f6993e49c015e2770526de0d449ee8b
Napat Poolpiputtanan <116888191+NapatPool@users.noreply.github.com>
2025-07-13 15:35:32 +0700

    Update Mini project SQL.sql

--- SQL/Mini project SQL.sql
+++ SQL/Mini project SQL.sql
@@ -1,11 +1,24 @@
 --Load chinook (Database).db to https://sqliteonline.com/
 
 -- select firstname lastname from customer table and select country is USA, Brazil, France 
+-- Excel vs SQL: Filter = Where
 select 
-	firstname AS fname, 
-    lastname as lname, 
+	firstname,
+    	lastname,
 	firstname  || lastname AS fullname
 FROM customers
-WHERE country in ('USA','Brazil','France')
+WHERE country in ('USA','Brazil','France');
+	
+--How to use pattern matching > Select whatever before "@yahoo" (use %)
+select 
+	firstname AS fname, 
+   	lastname AS lname, 
+	firstname,
+   	email
+FROM customers
+WHERE email LIKE '%@yahoo%';
+
+--How to use aggregate functions
+
 
 
