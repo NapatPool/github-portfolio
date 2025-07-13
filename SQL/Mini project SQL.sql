941a9f8c7a44b0394990d9d42fad52e058c356c9
Napat Poolpiputtanan <116888191+NapatPool@users.noreply.github.com>
2025-07-13 15:26:37 +0700

    Update Mini project SQL.sql

--- SQL/Mini project SQL.sql
+++ SQL/Mini project SQL.sql
@@ -1,1 +1,9 @@
+-- select firstname lastname from customer table and select country is USA, Brazil, France 
+select 
+	firstname AS fname, 
+    lastname as lname, 
+	firstname  || lastname AS fullname
+FROM customers
+WHERE country in ('USA','Brazil','France')
+
 
